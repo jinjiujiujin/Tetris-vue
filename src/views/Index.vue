82fde65c3e54f85f9bf5725e95371d681389f84e
jinjiujiujin <1179713734@qq.com>
2020-02-04 21:08:24 +0800

    version 1.0.0 最基本的玩法已实现

---
+++ src/views/Index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="container" class="bg">
+        <!--ui-->
+        <div id="ui_bg" class="ui_bg">
+            <div style="float:left;margin-right:4px;">
+                速度：<span>1</span>
+            </div>
+            <div style="float:left;">
+                当前分数：<span>0</span>
+            </div>
+            <div style="float:right;">
+                最高分数：<span>0</span>
+            </div>
+        </div>
+        <game-canvas ref="gameCanvas"></game-canvas>
+    </div>
+</template>
+
+<script>
+import GameCanvas from '@/components/GameCanvas.vue'
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      data: {
+        speed: 1,
+        cur_points: 0,
+        max_points: 0,
+        timer: null// 向下移动的定时器
+      }
+    }
+  },
+  components: {
+    GameCanvas
+  },
+  mounted () {
+    this.newGame()
+    // 绑定键盘监听事件
+    document.onkeydown = ({ keyCode }) => {
+      switch (keyCode) {
+        // 向下
+        case 40:// ↓
+        case 83:// S
+          this.next()
+          break
+        // 向左
+        case 37:// ←
+        case 65:// A
+          this.$refs.gameCanvas.moveLeft()
+          break
+        // 向右
+        case 39:// →
+        case 68:// D
+          this.$refs.gameCanvas.moveRight()
+          break
+        // 旋转
+        case 38:// ↑
+        case 87:// W
+          this.$refs.gameCanvas.rotate()
+          break
+      }
+    }
+  },
+  methods: {
+    newGame () {
+      this.cur_points = 0
+      this.speed = 1
+      this.$refs.gameCanvas.newGame()// 调用gameCanvas里的newGame
+      this.timer = setInterval(() => this.next(), 500 / this.speed)
+    },
+    next () { // 方块下移
+      if (this.$refs.gameCanvas.moveDown()) {
+        // 判断是否触顶
+        for (let i = 0; i < this.$refs.gameCanvas.currentFall.length; i++) {
+          if (this.$refs.gameCanvas.currentFall[i].y === 0) {
+            // gameEnd()
+            console.log('game end')
+            return
+          }
+        }
+        // 新的block
+        this.$refs.gameCanvas.createBlock()
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../assets/fonts/font.css';
+
+div>span{
+  font-family:tmb;
+  font-size:18pt;
+  color:green;
+}
+*{
+  margin:0;
+  padding:0;
+}
+.bg{
+  font-size:13pt;
+  background-color:rgb(239, 239, 227);
+  /*好看的渐变色*/
+  background-image:radial-gradient(rgb(239, 239, 227), rgb(230, 220, 212));
+  /*阴影*/
+  box-shadow:#cdc8c1 -1px -1px 7px 0px;
+  padding-bottom:4px;
+  margin:0 auto;
+}
+
+.ui_bg{
+  border-bottom:1px #a69e9ea3 solid;
+  padding-bottom:2px;
+  overflow:hidden;/*没有这句的话因为子div都设置了float，所以是浮在网页上的，所以父div就没有高度，这句清除了浮动，让父div有了子div的高度*/
+}
+</style>
